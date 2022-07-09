2d38f254d10a3920aca5293a9eb2c40cea24c4c3
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2022-07-08 19:10:19 -0600

    Primeros tests

--- src/test/java/com/emmanuelaguero/universidadbackend/UniversidadBackendApplicationTests.java
+++ src/test/java/com/emmanuelaguero/universidadbackend/UniversidadBackendApplicationTests.java
@@ -1,13 +1,43 @@
 package com.emmanuelaguero.universidadbackend;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
 class UniversidadBackendApplicationTests {
 
+
+    Calculador calculador = new Calculador();
+
+    class Calculador{
+        int suma(int a ,int b){
+            return a+b;
+        }
+    }
+
+
+
     @Test
-    void contextLoads() {
+    @DisplayName("Suma valor A y valor B")
+    void sumaDeValores() {
+        //given
+        int _a =2;
+        int _b =3;
+        //when
+        int expectativa = calculador.suma(_a,_b);
+        //then
+        int resultadoEsperado = 5;
+        assertThat(expectativa).isEqualTo(resultadoEsperado);
+    }
+
+    @Test
+    @Disabled("Test deprecado")
+    @DisplayName("Test deprecado")
+    void testDesactivado(){
     }
 
 }
