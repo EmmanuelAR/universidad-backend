f63b831e299dae37ee5d8b2b4fddb9e812f91019
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2022-07-14 12:12:55 -0600

    First use de loombok en carreramapper

--- src/main/java/com/emmanuelaguero/universidadbackend/modelo/dto/CarreraDTO.java
+++ src/main/java/com/emmanuelaguero/universidadbackend/modelo/dto/CarreraDTO.java
@@ -1,44 +1,13 @@
 package com.emmanuelaguero.universidadbackend.modelo.dto;
 
-public class CarreraDTO {
+import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarreraDTO {
     private Integer codigo;
     private String nombre;
     private Integer cantidad_materias;
     private Integer cantidad_anios;
-
-    public CarreraDTO() {
-    }
-
-    public Integer getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(Integer codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Integer getCantidad_materias() {
-        return cantidad_materias;
-    }
-
-    public void setCantidad_materias(Integer cantidad_materias) {
-        this.cantidad_materias = cantidad_materias;
-    }
-
-    public Integer getCantidad_anios() {
-        return cantidad_anios;
-    }
-
-    public void setCantidad_anios(Integer cantidad_anios) {
-        this.cantidad_anios = cantidad_anios;
-    }
 }
