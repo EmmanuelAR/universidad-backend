b41b24d87107b6325b5068a1c2bd136b6688bee5
EmmanuelAR <54730752+EmmanuelAR@users.noreply.github.com>
2022-07-17 11:43:15 -0600

    custom del swagger

--- src/main/java/com/emmanuelaguero/universidadbackend/controlador/dto/CarreraDTOController.java
+++ src/main/java/com/emmanuelaguero/universidadbackend/controlador/dto/CarreraDTOController.java
@@ -5,12 +5,11 @@ import com.emmanuelaguero.universidadbackend.modelo.entidades.Carrera;
 import com.emmanuelaguero.universidadbackend.modelo.mapper.CarreraMapper;
 import com.emmanuelaguero.universidadbackend.modelo.mapper.mapstruct.CarreraMapperMS;
 import com.emmanuelaguero.universidadbackend.servicios.contratos.CarreraDAO;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -20,6 +19,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/carreras")
 @ConditionalOnProperty(prefix = "app",name = "controller.enable-dto",havingValue = "true")
+@Api(value = "Acciones relacionadas con las carreras",tags = "Acciones sobre carreras.")
 public class CarreraDTOController extends GenericDTOController<Carrera,CarreraDAO> {
 
     @Autowired
@@ -30,6 +30,10 @@ public class CarreraDTOController extends GenericDTOController<Carrera,CarreraDA
     }
 
     @GetMapping
+    @ApiOperation(value = "Consultar todas las carreras.")
+    @ApiResponses({
+            @ApiResponse(code=200,message = "Ejecutado con exito.")
+    })
     public ResponseEntity<?> obtenerCarreras(){
         Map<String,Object> mensaje =new HashMap<>();
         //uso generico
@@ -46,5 +50,16 @@ public class CarreraDTOController extends GenericDTOController<Carrera,CarreraDA
         mensaje.put("success",Boolean.TRUE);
         mensaje.put("data",carreraDTOS);
         return ResponseEntity.ok(mensaje);
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "Consultar carrera por ID.")
+    public ResponseEntity<?> obtenerCarreraPorId(@PathVariable @ApiParam(name = "Codigo del sistema") Integer id){
+        return ResponseEntity.ok("");
+    }
+
+    @PostMapping
+    public ResponseEntity<?> saveCarrera(@RequestBody @ApiParam(name = "Carrera de la univiersidad") CarreraDTO carreraDTO){
+        return ResponseEntity.ok("");
     }
 }
--- src/main/java/com/emmanuelaguero/universidadbackend/modelo/dto/CarreraDTO.java
+++ src/main/java/com/emmanuelaguero/universidadbackend/modelo/dto/CarreraDTO.java
@@ -1,5 +1,7 @@
 package com.emmanuelaguero.universidadbackend.modelo.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
@@ -10,18 +12,23 @@ import javax.validation.constraints.Size;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(description = "Carrera de la universidad",value = "Carrera",reference = "Carrera")
 public class CarreraDTO {
 
+    @ApiModelProperty(name = "Codigo del sistema.",example = "5")
     private Integer codigo;
 
     @NotNull
     @NotEmpty(message = "Debe ingresar un valor.")
     @Size(min = 0,max = 80)
+    @ApiModelProperty(name = "Nombre de la carrera.",example = "Lic. en Alimentos",required = true)
     private String nombre;
 
     @Positive(message = "El valor no puede ser negativo") //validacion valor positivo
+    @ApiModelProperty(name = "Cantidad de materias de la carrera.",example = "50",required = true)
     private Integer cantidad_materias;
 
     @Positive(message = "El valor no puede ser negativo") //validacion valor positivo
+    @ApiModelProperty(notes = "Cantidad de años de la carrera.",example = "5",required = true)
     private Integer cantidad_anios;
 }
